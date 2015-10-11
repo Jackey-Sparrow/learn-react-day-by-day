b78dd69f48a22d3d00742cdcb6f0c5c3ea82cf4d
Jackey.Sparrow <1109952026@qq.com>
2015-10-11 23:18:11 +0800

    todo demo
    use setInterval to finish the todo demo,but still need to know how to
    communicate with different component

--- 08-ReactTodo/todo.js
+++ 08-ReactTodo/todo.js
@@ -4,22 +4,46 @@
 (function (React, ReactDOM) {
     'use strict';
 
+    var UserModel = function () {
+        this.userList = [
+            {
+                "id": 1,
+                "userName": "Jackey1",
+                "age": 26
+            },
+            {
+                "id": 2,
+                "userName": "Jackey2",
+                "age": 26
+            }
+        ];
+    };
+
+    UserModel.prototype.addUser = function (user) {
+        user.id = 3;//todo: get next id
+        this.userList.push(user);
+    };
+
+    UserModel.prototype.getUserList = function () {
+        return this.userList;
+    };
+
+    var userModel = new UserModel();
+
     var UserList = React.createClass({
+        loadUserList: function () {
+            this.setState({
+                userList: userModel.getUserList()
+            });
+        },
         getInitialState: function () {
             return {
-                userList: [
-                    {
-                        "id": 1,
-                        "userName": "Jackey1",
-                        "age": 26
-                    },
-                    {
-                        "id": 2,
-                        "userName": "Jackey2",
-                        "age": 26
-                    }
-                ]
+                userList: []//userModel.getUserList()
             };
+        },
+        componentDidMount: function () {
+            this.loadUserList();
+            setInterval(this.loadUserList, 1000);
         },
         render: function () {
             var users = this.state.userList.map(function (user) {
@@ -34,9 +58,26 @@
     });
 
     var AddUser = React.createClass({
+        handleSubmit: function (e) {
+            e.preventDefault();
+            var user = {
+                userName: this.refs.userName.value,
+                age: this.refs.age.value
+            };
+
+            userModel.addUser(user);
+        },
         render: function () {
             return (
-                <div>add User</div>
+                <div>
+                    <h2>add User</h2>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <input type='text' placeholder='userName' ref='userName'/>
+                        <input type='text' placeholder='age' ref='age'/>
+                        <input type='submit' value='Add User'/>
+                    </form>
+                </div>
             );
         }
     });
